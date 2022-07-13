68c9d2cbe10293101381e30890fd7bd82d23f3c6
Michael <michaelgrisso2011@gmail.com>
2022-07-13 11:47:57 -0400

    javascript

---
+++ js/currencyconverter.js
@@ -0,0 +1,6 @@
+function main(dollars) {
+    let euro = 0.88;
+    let conv = dollars * euro;
+    console.log(`${conv} EUR`);
+}
+main(17);
---
+++ js/inch2cm.js
@@ -0,0 +1,6 @@
+function main(inches) {
+    let cm = 2.54;
+    let result = inches * cm;
+    console.log(result);
+}
+main(21);
